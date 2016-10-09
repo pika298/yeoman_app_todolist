a6061e77c867f4334cdd322f69bdebc86f00984c
pika298 <kit2008@live.com>
2016-10-09 22:36:44 +0800

    Yeoman App - todolist complete

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,5 +2,16 @@
 
 angular.module('yeomanApp')
 	.controller('MainCtrl', function($scope) {
+		
 		$scope.todos = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'];
+		
+		$scope.addTodo = function() {
+			$scope.todos.push($scope.todo);
+			$scope.todo = '';
+		};
+
+		$scope.removeTodo = function(index) {
+			$scope.todos.splice(index, 1);
+		};
+
 });
